3d42043e34e3da06a3afb87244b27726c451a6e1
benhwlcode <141098928+benhwlcode@users.noreply.github.com>
2024-07-26 15:03:57 -0700

    initial commit v2

--- TicketAppTestingForm/TestWindow.cs
+++ TicketAppTestingForm/TestWindow.cs
@@ -1,10 +1,34 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TicketAppLibrary.Data;
+using TicketAppLibrary.DataAccess;
+
 namespace TicketAppTestingForm
 {
 	public partial class TestWindow : Form
 	{
-		public TestWindow()
+		private readonly IDatabaseData _db;
+
+		public TestWindow(ISQLDataAccess sQLDataAccess, IDatabaseData databaseData)
 		{
 			InitializeComponent();
 		}
+
+		private void InitializeAppSettings()
+		{
+			var services = new ServiceCollection();
+
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json");
+
+			IConfiguration config = builder.Build();
+
+			services.AddTransient<ISQLDataAccess, SQLDataAccess>();
+			services.AddTransient<IDatabaseData, SqlData>();
+
+
+		}
+
 	}
 }
